3e3811cf70827dc183dcc927f1e61e17b53cf3e3
s0l0n3t <furkantokgz@gmail.com>
2016-03-08 11:53:54 +0200

    Create lucky.py
    about luck .D test your change

---
+++ lucky.py
@@ -0,0 +1,32 @@
+#-*-coding:cp1254-*-
+import sys
+import random
+sys.path
+
+def __init__(self)
+       self.name = "root"
+       self.money=300
+       global self.money
+
+
+def main():
+      
+      rau =raw_input("dolars :")
+      rau=int
+      if not rau:
+              return
+      else:
+              a1 = raw_input(" integer :")
+              b1 = random.randint(1,7)
+                   if (a1 ==b1):
+                        sy =rau*2
+                        print "succes !"
+                        self.money + sy 
+                        print self.money
+                        return
+                    else:
+                         self.money - rau
+                         priht self.money
+                         return
+
+ 
