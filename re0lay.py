81225a2bce7f08d22b483df9b344482cd4acd66a
s0l0n3t <furkantokgz@gmail.com>
2016-03-26 11:19:20 +0200

    Added files via upload
    Nice one :))
     this is saving.

---
+++ re0lay.py
@@ -0,0 +1,54 @@
+#-*-coding:cp1254-*-
+#s0l0n3t
+import sys
+sys.path
+import os
+import random
+import time
+
+
+class sepp:
+    def __init__(self):
+        self.system = os.name
+        self.url = "https://realpython.com/learn/python-first-steps/images/pythonlogo.jpg"
+        self.safely =os.chdir("C://Users/bilgisayar/Desktop")
+        self.abc = ["a","b","c","d","e","f","g","h","j","k","l","m","n","o","p","r","s","t","v","y","z"]
+        self.number = ["1","2","3","4","5","6","7","8","9","0"]
+	
+
+slain = sepp()
+
+
+def dosya():
+	for i in seath:
+	    print a1 + i
+	dosya = open("hash.txt","a")
+	dosya.write("\n%s\n"%(a1 + i))
+	dosya.close
+
+def pas():
+ try:
+    if slain.system  in "nt":
+        print "for exit :Ctrl + C\n"
+        print 
+    seath = []
+    a1 =(random.choice(slain.number)+random.choice(slain.abc))
+    global seath,a1
+    return seath.append("".join(random.choice(slain.abc)+random.choice(slain.abc)+random.choice(slain.abc)+random.choice(slain.abc)+random.choice(slain.abc)+random.choice(slain.number)+random.choice(slain.number)+random.choice(slain.abc)+random.choice(slain.abc)+random.choice(slain.abc)+random.choice(slain.number)+random.choice(slain.number)+random.choice(slain.abc)+random.choice(slain.number)))
+ except KeyboardInterrupt:
+        timi =int(time.clock())
+        print "Second : %s"%(timi - tim)
+        sys.exit()
+
+tim = int(time.clock())
+global tim
+
+while True:
+    pas()
+    dosya()
+    continue
+    
+    
+    
+    
+	
