ab2ce9bece79bab9ba72e8cde3f59926bf903b66
s0l0n3t <furkantokgz@gmail.com>
2016-04-11 18:59:00 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 #-*-coding:cp1254-*-
+#github.com/s0l0n3t/modules
 import sys
 import random
 sys.path
@@ -7,12 +8,15 @@ logo = """
        Amac:
          Zarin sayisini bilmektir.
          Eger bilirseniz verdiginiz miktarin 2 katini kazanirsiniz.
-**Komutlar icin Kac tl sorusuna komutlar yazınız."""
+      
+**Komutlar icin Kac tl sorusuna komutlar yazınız. 
+						  """
 global money,name
 
 print logo
+print
 class database():
-       name = "Ali"
+       name = "User"
        money = 300
 
 database=database()
@@ -24,7 +28,6 @@ def main():
                     if not rau:
                             return
                     else:
-              
                      a1 = input(" (1-6 arasi)sayi :")
                      b1 = random.randrange(1,7)
                      if (a1 == b1):
@@ -49,9 +52,35 @@ def main():
              elif soru == "bakiyem":
                 print database.money
              elif soru == "komutlar":
-                    print "isim için --> isim"
+                    print "isim icin --> isim"
                     print
-                    print "Bakiyenizi ogrenmek için --> param,miktar,para,bakiyem"
+                    print "Bakiyenizi ogrenmek için --> bakiyem"
+		    print
+		    print "Kayit islemleri icin --> kayit"
+		    print
+		    print "Sifirlama islemi icin --> sifirla" 
+	     elif soru == "sifirla":
+	       database.money = 300
+             elif soru == "kayit":
+		  user = raw_input("kaydet yada ac :")
+		  if user == "kaydet":
+		      dosya = open("%s"%(database.name),"w")
+		      dosya.write(int("%s"%(database.money)))
+		      dosya.close()
+		      print "başarılı."
+		      return
+		  if user == "ac":
+		     user1 = raw_input("Kullanici adi :")
+		     if not user1:
+			print "Lutfen bir kullanici adi girin."
+			return
+		     else:
+			dosyam = open(user1)
+			liste = dosyam.read()
+			user1 = database.name
+			database.money = liste
+			print "%s kullanicisi bilgisi eklenmistir.(%s)"%(user1,liste)
+			return
              else:
                  return
              
