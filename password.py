3688baaf713ee65674baabb6d18d8fd7f6cfacd2
s0l0n3t <furkantokgz@gmail.com>
2016-04-11 19:28:36 +0300

    Update password.py

--- password.py
+++ password.py
@@ -1,60 +1,55 @@
-#-*-coding:cp1254-*-
-import sys
+#-*-coding: cp1254-*-
+#github.com/s0l0n3t/modules
+
+import os
 import random
+import sys
 sys.path
 
-logo = """
-       Amac:
-         Zarin sayisini bilmektir.
-         Eger bilirseniz verdiginiz miktarin 2 katini kazanirsiniz.
-**Komutlar icin Kac tl sorusuna komutlar yazınız."""
-global money,name
 
-print logo
-class database():
-       name = "Ali"
-       money = 300
+print """
+        Enter Anything."""
+print
 
-database=database()
+
+def read(key):
+    if (os.name == "nt"):
+        dosya = open("key.txt","a")
+        dosya.write("\n%s" %(key))
+        dosya.close()
+    if (os.name == "posix"):
+        dosya = open("key","a")
+        dosya.write("\n%s" %(key))
+        dosya.close()
 
 def main():
-             soru = raw_input(" :")
-             if not soru:
-                    rau =input("Kac tl :")
-                    if not rau:
-                            return
-                    else:
-              
-                     a1 = input(" (1-6 arasi)sayi :")
-                     b1 = random.randrange(1,7)
-                     if (a1 == b1):
-                        sy =rau*2
-                        print "Kazandin. Verdigin miktarin iki kati eklendi."
-                        database.money+= sy 
-                        print database.money
-                        return
-                     else:
-                         print "Kaybettin. Verdigin miktar kadar azaldi."
-                         database.money -= rau
-                         print database.money
-                         return
-             elif soru == "isim":
-                     print database.name
-                     nick = raw_input("Yeni isim :")
-                     if not nick:
-                            return
-                     else:
-                            database.name = nick
-                            return
-             elif soru == "bakiyem":
-                print database.money
-             elif soru == "komutlar":
-                    print "isim için --> isim"
-                    print
-                    print "Bakiyenizi ogrenmek için --> param,miktar,para,bakiyem"
-             else:
-                 return
-             
+        laps = ["a","b","c","d","e","f","g","h","j","k","l","m","n","p","r","s","t","v","w","x","v","z"]
+        maps = ["@","$","?","!","#","%","&"]
+        second = ["1","2","3","4","5","6","7","8","9","0"]
+        a1=random.choice(laps)
+        a2=random.choice(laps)
+        a3=random.choice(laps)
+        a5=random.choice(maps)
+        a6=random.choice(second)
+        a7=random.choice(second)
+        a8=random.choice(second)
+        a9=random.choice(second)
+        a4=random.choice(laps)
+        rains = [a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9]
+       	for i in rains:
+        	print "Hash : %s"%(i)
+		global i
+
+def again():
+    
+    a1 = raw_input(":")
+    if (a1 == "*"):
+        return
+    else:
+        main()
+        read(i)
+        return
 while True:
-       
-       main()
+
+    again()
+    
