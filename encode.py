6c763285afaac9d47f340576fe54a6136df0ab39
s0l0n3t <furkantokgz@gmail.com>
2016-04-11 19:59:02 +0300

    use with decode

--- encode.py
+++ encode.py
@@ -1,29 +1,28 @@
 #-*-coding:cp1254-*-
-import os
 import sys
 sys.path
 
-print """Dosya okutmayacaksan boş bırak. """
+print """Dosya okutmayacaksan bos birak. """
 
 def encode():
     sy = raw_input("okunacak dosya :")
     if sy is "":
-        msg =raw_input("şifre :")
+        msg =raw_input("sifre :")
         sifreli_msg="""%s"""%(msg)
     else:
         
         sifreli_msg="""%s"""%(sy)
     
 
-    sozluk = {"a":"z", "b":"v" , "c":"w" ,
+    sozluk = {"a":"z", "b":"v" , "0":"w" ,
               "d":"l", "e":"n" , "f":"i" ,
-              "g":"h", "ç":"s" , "ğ":"a" ,
-              "h":"g", "i":"f" , "ı":"ç" ,
-              "j":"d", "k":"j" , "l":"k" ,
+              "g":"h", "c":"s" , "i":"a" ,
+              "h":"g", "w":"c" ,"j":"f",
+              "k":"j" , "l":"k" ,
               "m":"t", "n":"b" , "o":"e" ,
-              "ö":"m", "p":"j" , "r":"p" ,
-              "s":"ö", "ş":"s" , "t":"c" ,
-              "u":"ş", "ü":"ı" , "v":"k" ,
+              "t":"m", "p":"0" , "r":"p" ,
+              "s":"u",
+              "u":"d", "v":"y" ,
               "y":"o", "z":"r"}
 
     sifresiz_metin = ""
@@ -35,8 +34,8 @@ def encode():
     global sifresiz_metin
 
 def yaz():
-    sor = raw_input("dosya adı :")
-    dosya = open(sor,"a")
+    sor = raw_input("yazilacak dosya adi :")
+    dosya = open(sor+".txt","a")
     dosya.write(":%s"%(sifresiz_metin))
     dosya.close
 
