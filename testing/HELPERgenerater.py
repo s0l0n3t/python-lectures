079dc16a2da84bce2333d8f6265debf9ff68610f
s0l0n3t <furkantokgz@gmail.com>
2016-05-29 17:24:02 +0300

    Update HELPERgenerater.py

--- testing/HELPERgenerater.py
+++ testing/HELPERgenerater.py
@@ -1,42 +1,42 @@
 #-*-coding:cp1254-*-
 #!/usr/bin python2
+#github.com/s0l0n3t/modules
 
 import os
 import time
 
-print """ //\    /}        |  /\   
-         //  \  / |s0l0n3t | /  \
-        //    \/  |        |/    \
-        /
-"""
+logo =""" //\    /bin            *     /\   /tmp
+         //  \  / s0l0n3t  /  \  /      /mnt
+        // /usr   \/     *     /    \ /     /etc
+        /  /                 """
+print logo
 
 class user():
  name = None
  year = None
- num = ["1","2","3","4","5","6","7","8","9"]
- spec = None
+ special = None
 	
 user = user()	
-	
+#Burada kullanım açısından daha iyi olması amacıyla sınıfa aldım.
+
 def ask():
- isim = raw_input("Name >>")
- user.name = isim
- yas = raw_input("Year >>")
- user.year = yas
- special = raw_input("Special >>")
- user.spec = special
-	
+ dname = raw_input("INPUT[1] >> ")
+ user.name = dname
+ year = raw_input("INPUT[2] >> ")
+ user.year = dyear
+ dspecial = raw_input("INPUT[3] >> ")
+ user.spec = dspecial
+#Kullanıcıdan 3 giriş alıyor.
+
 def information():
   hight1 = user.name[0].upper() + user.name[1:]
-  low = [user.name, user.year, user.spec]
-  low.append(hight1)
+  low = [user.name, user.year, user.spec ,hight1]
   global low
   
 def write():
- password =[]
  pas1 =low[0] + low[1]
- pas2 = low[0] + low[2]
- pas3 = low[0] + low[3]
+ pas2 =low[0] + low[2]
+ pas3 =low[0] + low[3]
  pas4 =low[1] + low[0]
  pas5 =low[1] + low[1]
  pas6 =low[1] + low[2]
@@ -49,22 +49,9 @@ def write():
  pas13 =low[3] + low[1]
  pas14 =low[3] + low[2]
  pas15 =low[3] + low[3]
- password.append(pas1)
- password.append(pas2)
- password.append(pas3)
- password.append(pas4)
- password.append(pas5)
- password.append(pas6)
- password.append(pas7)
- password.append(pas8)
- password.append(pas9)
- password.append(pas10)
- password.append(pas11)
- password.append(pas12)
- password.append(pas13)
- password.append(pas14)
- password.append(pas15)
- dosya.write("WORDS_")
+#Permütasyonlar
+ time.sleep(0.1)
+ dosya.write("\nWORDS_")
  dosya.write("\n{}".format(pas1))
  dosya.write("\n{}".format(pas2))
  dosya.write("\n{}".format(pas3))
@@ -80,21 +67,23 @@ def write():
  dosya.write("\n{}".format(pas13))
  dosya.write("\n{}".format(pas14))
  dosya.write("\n{}".format(pas15))
-
+#Tün permütasyonları tek tek yazıp, dosyaya yazdırıyor.
 
 def crate():
   if "words.txt" not in os.listdir(os.path.abspath("")):
-        dosya = open("words.txt","w")
-        global dosya
+          dosya = open("words.txt","a")
+          global dosya
   else:
-      print "[!] words.txt in path"
-      print
-      dosya = open("words.txt","w")
-      global dosya
+          print "[!] words.txt in path"
+          print
+          num = "1"
+          dosya = open("words"+ num + ".txt","a")
+          global dosya
+#Eğer path üzerinde words.txt dosyası var ise oluşturuyor yoksa başına 1 ekliyor.
 
-while True :
- ask()
- information()
- crate()
- write()
+ask()
+information()
+crate()
+write()
+#While döngüsü koymadım çünkü GUI kapanmadan yazmıyor.
 
